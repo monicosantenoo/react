2e549789c02c80f04de686cae82f6acce15125ff
monicosantenoo <manirautela555@gmail.com>
2025-09-01 22:12:22 +0530

    okay

---
+++ vite_project13/src/App.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+return(
+<>
+<ol>
+  <li>react is a popular library</li>
+  <li> react is most powerfull framework</li>
+  <li> i am more react likely to get a job a front and devloper </li>
+</ol>
+</>
+)
+}
+
+export default App
